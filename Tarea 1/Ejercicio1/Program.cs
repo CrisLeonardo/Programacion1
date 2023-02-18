860def48b8515dee69082142c049ee4e6e1920bd
Cristopher Leonardo Zepeda Esquivel (Leo) <crisleonardo100@gmail.com>
2023-02-17 19:08:06 -0600

    Add files via upload

---
+++ Tarea 1/Ejercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Que edad tienes? ");
+        byte edad = byte.Parse(Console.ReadLine());
+        Console.WriteLine("No pareces tener {0} años.", edad);
+    }
+        }
