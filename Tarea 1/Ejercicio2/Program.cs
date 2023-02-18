993c2620769fd059ec72c1d97f6c48ea411ffbf0
Cristopher Leonardo Zepeda Esquivel (Leo) <crisleonardo100@gmail.com>
2023-02-17 19:51:32 -0600

    Add files via upload

---
+++ Tarea 1/Ejercicio2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese el 1er numero de dos cifras: ");
+        byte n1 = Byte.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el 2do numero de dos cifras: ");
+        byte n2 = Byte.Parse(Console.ReadLine());
+        ushort resultado = (ushort)(n1 * n2);
+
+        Console.WriteLine("El total de la Multiplicacion de las dos cantidades es: " + resultado);
+    }
+}
---
+++ Tarea 1/Ejercicio3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+{
+    long n1, n2;
+
+    Console.WriteLine("Ingresar numeros mayores a 19 porfavor.\n");
+    Console.WriteLine("Ingrese el 1er numero: ");
+    n1 = long.Parse(Console.ReadLine());
+    Console.WriteLine("Ingresar el 2do numero: ");
+    n2 = long.Parse(Console.ReadLine());
+
+    long suma = n1 + n2;
+    long resta = n1 - n2;
+    long multi = n1 * n2;
+
+    Console.WriteLine($"Entonces {n1} + {n2} es: {suma} ");
+    Console.WriteLine($"Entonces {n1} - {n2} es: {resta} ");
+    Console.WriteLine($"Entonces {n1} * {n2} es: {multi} ");
+
+    Console.ReadKey();
+
+}
+
